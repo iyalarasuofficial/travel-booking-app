f84033af2f0de8fd922a95b7e49164003fa5165d
iyalarasuofficial <iyalarasuofficial@gmail.com>
2025-01-16 13:12:27 +0530

    sucess

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Additional configurations (optional)
+  build: {
+    // Enable assets optimization
+    assetsInlineLimit: 4096, // Files smaller than 4kb will be inlined as data URIs
+  },
 })
